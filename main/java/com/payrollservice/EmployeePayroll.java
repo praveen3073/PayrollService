9fc32611f4fe59d4bbca09c414d7cddbaaee5bf8
praveen3073 <pop3073@gmail.com>
2020-10-29 21:34:30 +0530

    Added: UC2 Data Retreival From DB

---
+++ main/java/com/payrollservice/EmployeePayroll.java
@@ -0,0 +1,37 @@
+package com.payrollservice;
+
+import java.util.Date;
+
+public class EmployeePayroll {
+    int company_id;
+    int emp_id;
+    String name;
+    String phone;
+    String address;
+    char gender;
+    Date start;
+    double basic_pay;
+    double deductions;
+    double taxable_pay;
+    double tax;
+    double net_pay;
+
+    public EmployeePayroll() {
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayroll { company_id : " + company_id + ", " +
+                                 " emp_id : " + emp_id + ", " +
+                                 " name : " + name + ", " +
+                                 " phone : " + phone + ", " +
+                                 " address : " + address + ", " +
+                                 " gender : " + gender + ", " +
+                                 " date : " + start + ", " +
+                                 " basic_pay : " + basic_pay + ", " +
+                                 " deductions : " + deductions + ", " +
+                                 " taxable_pay : " + taxable_pay + ", " +
+                                 " tax : " + tax + ", " +
+                                 " net_pay: " + net_pay + " }";
+    }
+}
--- main/java/com/payrollservice/PayrollService.java
+++ main/java/com/payrollservice/PayrollService.java
@@ -3,32 +3,57 @@ package com.payrollservice;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
+import java.util.HashMap;
 import java.util.Properties;
 
 public class PayrollService {
-    private static Connection getConnection() throws IOException, SQLException {
+    HashMap<Integer, EmployeePayroll> employeePayrollMap = new HashMap<>();
+
+    private Connection getConnection() throws IOException, SQLException {
         FileInputStream fin = new FileInputStream("C:\\Users\\Praveen Satya\\IdeaProjects\\PayrollServiceProject\\config.properties");
         Properties prop = new Properties();
         prop.load(fin);
         return DriverManager.getConnection(prop.getProperty("db.url"), prop.getProperty("db.user"), prop.getProperty("db.password"));
     }
+
     public static void main(String[] args) {
+        PayrollService payrollService = new PayrollService();
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        Connection con = null;
+        Connection con;
         try {
-            con = getConnection();
+            con = payrollService.getConnection();
             Statement stmt = con.createStatement();
-            ResultSet resultSet = stmt.executeQuery("select * from employee");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("name"));
-            }
+            payrollService.getRecordsFromDB(payrollService, stmt);
         } catch (IOException | SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void getRecordsFromDB(PayrollService payrollService, Statement stmt) throws SQLException {
+        String query = "select * from employee e, payroll p " +
+                "where e.emp_id = p.emp_id";
+        ResultSet resultSet = stmt.executeQuery(query);
+        while (resultSet.next()) {
+            EmployeePayroll tempLoopObject = new EmployeePayroll();
+            tempLoopObject.company_id = resultSet.getInt("company_id");
+            tempLoopObject.emp_id = resultSet.getInt("emp_id");
+            tempLoopObject.name = resultSet.getString("name");
+            tempLoopObject.phone = resultSet.getString("phone");
+            tempLoopObject.address = resultSet.getString("address");
+            tempLoopObject.gender = resultSet.getString("gender").charAt(0);
+            tempLoopObject.start = resultSet.getDate("start");
+            tempLoopObject.basic_pay = resultSet.getDouble("basic_pay");
+            tempLoopObject.deductions = resultSet.getDouble("deductions");
+            tempLoopObject.taxable_pay = resultSet.getDouble("taxable_pay");
+            tempLoopObject.tax = resultSet.getDouble("tax");
+            tempLoopObject.net_pay = resultSet.getDouble("net_pay");
+            payrollService.employeePayrollMap.put(tempLoopObject.emp_id, tempLoopObject);
+            System.out.println(tempLoopObject);
         }
     }
 }
